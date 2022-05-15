d675973cd061010bda89353a2f6ffa7e06300b03
Youssef Saeed <81388596+Y0u553f5433d@users.noreply.github.com>
2022-05-16 00:41:15 +0200

    Update host-discovery.py

--- host-discovery.py
+++ host-discovery.py
@@ -4,14 +4,11 @@ import ipaddress
 import os
 
 
-#ip address string must be in unicode to use ipaddress module
 network = ipaddress.ip_network(unicode("10.11.1.0/24"))
-#network = ipaddress.ip_network(unicode(input("What's your Network > ")))
 print("Python Host Discovery on 10.11.1.0/24")
 
 for i in network.hosts():
-    #str(i)
-
+    
     response = os.system("ping %s -c 1 > /dev/null" %i)
 
     if response == 0:
