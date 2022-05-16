141e9196c70cc2112720effdaadc766930cfd00a
Youssef Saeed <81388596+Y0u553f5433d@users.noreply.github.com>
2022-05-16 10:52:15 +0200

    Create log-sorter.py

---
+++ log-sorter.py
@@ -0,0 +1,12 @@
+import re
+
+def log_reader(file):
+    with open(file) as fd:
+        for line in fd:
+            if re.match("(.*)\.js", line):
+                yield line.split()[6].split('/')[2]
+my_reader = log_reader('access_log.txt')
+paths = set(my_reader) # unique elements
+sortt=sorted(paths)
+for row in sortt:
+    print(row)
